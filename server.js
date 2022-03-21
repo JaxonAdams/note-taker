9fe17284b14847cbaa7c230de23ecdf74b3ce4c7
Jaxon Adams <jadamsresume1@gmail.com>
2022-03-21 12:01:24 -0600

    /notes and /notes/:id endpoints

--- server.js
+++ server.js
@@ -4,8 +4,23 @@ const { notes } = require('./db/db.json');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+function findById(id, notesArr) {
+    const result = notesArr.filter(note => note.id === id)[0];
+    return result;
+};
+
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
